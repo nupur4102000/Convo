a7abc59fb0efa1b46541c453898583ba98d0bc77
Nupur Gupta <91957347+nupur4102000@users.noreply.github.com>
2022-07-11 22:29:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -75,27 +75,27 @@
          }
        };
      
-//        useEffect(() => {
-//          const q = query(collection(db, "Messages"), orderBy("createdAt", "asc"));
+       useEffect(() => {
+         const q = query(collection(db, "Messages"), orderBy("createdAt", "asc"));
      
-//          const unsubscribe = onAuthStateChanged(auth, (data) => {
-//            setUser(data);
-//          });
+         const unsubscribe = onAuthStateChanged(auth, (data) => {
+           setUser(data);
+         });
      
-//          const unsubscribeForMessage = onSnapshot(q, (snap) => {
-//            setMessages(
-//              snap.docs.map((item) => {
-//                const id = item.id;
-//                return { id, ...item.data() };
-//              })
-//            );
-//          });
+         const unsubscribeForMessage = onSnapshot(q, (snap) => {
+           setMessages(
+             snap.docs.map((item) => {
+               const id = item.id;
+               return { id, ...item.data() };
+             })
+           );
+         });
      
-//          return () => {
-//            unsubscribe();
-//            unsubscribeForMessage();
-//          };
-//        }, []);
+         return () => {
+           unsubscribe();
+           unsubscribeForMessage();
+         };
+       }, []);
      
        return (
          <Box bg={"red.50"}>
